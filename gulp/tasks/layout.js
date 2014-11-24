799202c8fb523f0362570e0db056c4dfc646cb0a
shawnsandy <shawnsandy04@gmail.com>
2014-11-24 13:49:09 -0500

    working on integrating web-components

--- gulp/tasks/layout.js
+++ gulp/tasks/layout.js
@@ -8,7 +8,7 @@ var gulp = require('gulp'),
 
 gulp.task('include_layouts', function () {
 
-    gulp.src( config.layouts.srcHtml, {
+    gulp.src( config.layouts.srcFiles, {
         base: config.layouts.src
     })
         .pipe(fileinclude({
@@ -22,7 +22,6 @@ gulp.task('include_layouts', function () {
 gulp.task('layout:watch', ['browser-sync'],  function () {
     gulp.watch(config.layouts.srcCss, ['browser-sync:notify'])
     gulp.watch(config.layouts.srcHtml, ['include_layouts', 'browser-sync:notify'])
-
 });
 
 gulp.task('layout', ['include_layouts','browser-sync'], function () {
